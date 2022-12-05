92e0267a0d1345d93d5207ec6c2ebd525ceb27b1
AidarT <75251421+AidarT@users.noreply.github.com>
2022-12-05 16:05:17 +0500

    day 4

--- aoc2022day4.py
+++ aoc2022day4.py
@@ -4,8 +4,7 @@ with open('C:/Users/User/Documents/input.txt') as f:
     my_input = [list(map(lambda a: int(a), re.findall(r'\d+', line))) for line in f.read().split('\n')]
 f.close()
 
-part1 = 0
-part2 = 0
+part1, part2 = 0, 0
 for line in my_input:
     if ((line[2] <= line[1] and line[2] >= line[0] and line[3] <= line[1] and line[3] >= line[0]) or 
     (line[0] <= line[3] and line[0] >= line[2] and line[1] <= line[3] and line[1] >= line[2])):

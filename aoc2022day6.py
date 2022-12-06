9836f7e5f86330f2a01a57c544d041e7c442cb9d
AidarT <75251421+AidarT@users.noreply.github.com>
2022-12-06 11:16:32 +0500

    day 6

---
+++ aoc2022day6.py
@@ -0,0 +1,19 @@
+with open('C:/Users/User/Documents/input.txt') as f:
+    my_input = f.read()
+f.close()
+
+part1 = 0
+for i in range(0, len(my_input)):
+    marker = my_input[i:i+4]
+    if len(list(set(marker))) == 4:
+        part1 = i + 4
+        break
+
+part2 = 0
+for i in range(0, len(my_input)):
+    marker = my_input[i:i+14]
+    if len(list(set(marker))) == 14:
+        part2 = i + 14
+        break
+
+print(str(part1) + " " + str(part2))

8ef835c58ce150da2990326eb997eaf956c6a845
AidarT <75251421+AidarT@users.noreply.github.com>
2022-12-06 11:35:17 +0500

    day 6

--- aoc2022day6.py
+++ aoc2022day6.py
@@ -3,14 +3,14 @@ with open('C:/Users/User/Documents/input.txt') as f:
 f.close()
 
 part1 = 0
-for i in range(0, len(my_input)):
+for i in range(0, len(my_input)-4):
     marker = my_input[i:i+4]
     if len(list(set(marker))) == 4:
         part1 = i + 4
         break
 
 part2 = 0
-for i in range(0, len(my_input)):
+for i in range(0, len(my_input)-14):
     marker = my_input[i:i+14]
     if len(list(set(marker))) == 14:
         part2 = i + 14
